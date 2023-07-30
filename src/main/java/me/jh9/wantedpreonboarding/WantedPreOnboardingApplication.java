9eec3b0f11d2586b8ad2325b753a7c0dc92eec80
Nine-JH <rnwkgus2@gmail.com>
2023-07-30 20:46:14 +0900

    [init] 프로젝트 생성

---
+++ src/main/java/me/jh9/wantedpreonboarding/WantedPreOnboardingApplication.java
@@ -0,0 +1,13 @@
+package me.jh9.wantedpreonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedPreOnboardingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jh9/wantedpreonboarding/WantedPreOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jh9.wantedpreonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreOnboardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
