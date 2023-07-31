40347f7fedd488c88851070b11a8a679f9abeb3f
Nine-JH <rnwkgus2@gmail.com>
2023-07-31 10:49:45 +0900

    [feat] Member.class 생성 * id 식별자 값 * email 값 * 비밀번호 * Member을 생성하는 static method
    * TODO : 비밀번호 암호화

---
+++ src/test/java/me/jh9/wantedpreonboarding/member/unit/domain/MemberTest.java
@@ -0,0 +1,34 @@
+package me.jh9.wantedpreonboarding.member.unit.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import me.jh9.wantedpreonboarding.member.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class MemberTest {
+
+    @DisplayName("createNewMember() 는")
+    @Nested
+    class Context_CreateNewMember {
+
+        @DisplayName("적절한 값 입력 시 새로운 Member 생성이 가능하다.")
+        @Test
+        void _willSuccess() {
+            // given
+            String correctEmail = "test@email.com";
+            String correctPassword = "password";
+
+            // when
+            Member newMember = Member.createNewMember(correctEmail, correctPassword);
+
+            // then
+            Assertions.assertThat(newMember).extracting("email", "password")
+                .containsExactly(correctEmail, correctPassword);
+
+            Assertions.assertThat(newMember.getId()).isNull();
+        }
+    }
+}
