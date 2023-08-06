c0ce7f66e1ad3daa8137eed887009a5d8beedf8b
Nine-JH <rnwkgus2@gmail.com>
2023-08-06 22:00:52 +0900

    [feat] ControllerUnitTestSupport.java 변경
    * Controller들의 UnitTest들은 SpringSecurity에 영향을 받으면 안되므로 exclude 처리.

--- src/test/java/me/jh9/wantedpreonboarding/utils/ControllerUnitTestSupport.java
+++ src/test/java/me/jh9/wantedpreonboarding/utils/ControllerUnitTestSupport.java
@@ -3,18 +3,23 @@ package me.jh9.wantedpreonboarding.utils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.jh9.wantedpreonboarding.article.api.ArticleController;
 import me.jh9.wantedpreonboarding.article.application.ArticleService;
-import me.jh9.wantedpreonboarding.common.jwt.application.JwtService;
 import me.jh9.wantedpreonboarding.common.jwt.api.JwtController;
+import me.jh9.wantedpreonboarding.common.jwt.application.JwtService;
 import me.jh9.wantedpreonboarding.member.api.MemberController;
 import me.jh9.wantedpreonboarding.member.application.MemberService;
+import me.jh9.wantedpreonboarding.utils.config.UnitTestConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
+@Import(UnitTestConfig.class)
 @WebMvcTest(
-    controllers = {MemberController.class, JwtController.class, ArticleController.class})
-public abstract class ControllerTestSupport {
+    controllers = {MemberController.class, JwtController.class, ArticleController.class},
+    excludeAutoConfiguration = SecurityAutoConfiguration.class)
+public abstract class ControllerUnitTestSupport {
 
     @Autowired
     protected MockMvc mockMvc;
@@ -26,8 +31,8 @@ public abstract class ControllerTestSupport {
     protected MemberService memberService;
 
     @MockBean
-    protected JwtService jwtService;
+    protected ArticleService articleService;
 
     @MockBean
-    protected ArticleService articleService;
+    protected JwtService jwtService;
 }
