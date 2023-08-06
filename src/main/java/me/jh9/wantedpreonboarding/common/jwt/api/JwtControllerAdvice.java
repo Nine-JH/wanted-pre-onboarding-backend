59bafd492a6f1688913008439a8c363df9a722ed
Nine-JH <rnwkgus2@gmail.com>
2023-08-06 22:17:29 +0900

    [feat] 통합 및 인수테스트용 TestContainers 추가 * mysql, redis container 추가

--- src/main/java/me/jh9/wantedpreonboarding/common/jwt/api/JwtControllerAdvice.java
+++ src/main/java/me/jh9/wantedpreonboarding/common/jwt/api/JwtControllerAdvice.java
@@ -3,17 +3,14 @@ package me.jh9.wantedpreonboarding.common.jwt.api;
 import me.jh9.wantedpreonboarding.common.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(basePackages = "me.jh9.wantedpreonboarding.common.jwt.api")
+@RestControllerAdvice
 public class JwtControllerAdvice {
-
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> bindException(BindException exception) {
+    public ResponseEntity<ErrorResponse> jwtDenied(RuntimeException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(new ErrorResponse(
-                exception.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
+            .body(new ErrorResponse(exception.getMessage()));
     }
 }
