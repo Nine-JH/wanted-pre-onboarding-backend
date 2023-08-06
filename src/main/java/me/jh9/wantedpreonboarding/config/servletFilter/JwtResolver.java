ecaa2b9ab1a410b1fdc2ca144a059818fa8462b7
Nine-JH <rnwkgus2@gmail.com>
2023-08-06 22:00:07 +0900

    [feat] Jwt Resolver 추가
    * 이제 각각의 요청 body에서 member_id를 받지 않고, JWT에서 추출해서 사용하기 위해 Resolver 추가.

---
+++ src/main/java/me/jh9/wantedpreonboarding/config/servletFilter/JwtResolver.java
@@ -0,0 +1,24 @@
+package me.jh9.wantedpreonboarding.config.servletFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import me.jh9.wantedpreonboarding.common.jwt.infra.JwtInfo;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class JwtResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(Long.class) && parameter.hasParameterAnnotation(JwtInfo.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        return request.getAttribute("memberId");
+    }
+}
